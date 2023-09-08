d2df25d3e1e590d12a7ff3f482baddb68cfc88b2
portal222 <dvaportala@gmail.com>
2023-09-08 14:53:47 +0200

    sekunde

--- src/components/Tajmer.js
+++ src/components/Tajmer.js
@@ -8,8 +8,12 @@ const Tajmer = () => {
     const [isOpen1, setOpen1] = useState(false)
     const [isOpen2, setOpen2] = useState(false)
     const [isOpen3, setOpen3] = useState(false)
+    const [inputValue1, setInputValue1] = useState(0);
 
 
+    const handleChangeValue1 = (e) => {
+        setInputValue1(e.target.value)
+    }
 
     return (
         <>
@@ -20,48 +24,48 @@ const Tajmer = () => {
                         <motion.div
                             className="box"
                             layout
-                            style={{ height: isOpen ? "300px" : "15px" }}
-                            transition={{ duration: 3 }}
+                            style={{ height: isOpen ? "315px" : "0px" }}
+                            transition={{ duration: 5 }}
 
                         />
                         <div onClick={() => setOpen(!isOpen)}
                             className="boxBat"
-                        >3 SEK</div>
-
+                        >5 sek</div>
+                       
                     </div>
 
                     <div>
                         <motion.div
                             className="box1"
                             layout
-                            style={{ height: isOpen1 ? "300px" : "15px" }}
-                            transition={{ duration: 5 }}
+                            style={{ height: isOpen1 ? "315px" : "0px" }}
+                            transition={{ duration: 10 }}
                         />
                         <div onClick={() => setOpen1(!isOpen1)}
                             className="boxBat"
-                        >5 SEK</div>
+                        >10 sek</div>
                     </div>
                     <div>
                         <motion.div
                             className="box2"
                             layout
-                            style={{ height: isOpen2 ? "300px" : "15px" }}
-                            transition={{ duration: 8 }}
+                            style={{ height: isOpen2 ? "315px" : "0px" }}
+                            transition={{ duration: 20 }}
                         />
                         <div onClick={() => setOpen2(!isOpen2)}
                             className="boxBat"
-                        >8 SEK</div>
+                        >20 sek</div>
                     </div>
                     <div>
                         <motion.div
                             className="box3"
                             layout
-                            style={{ height: isOpen3 ? "300px" : "15px" }}
-                            transition={{ duration: 13 }}
+                            style={{ height: isOpen3 ? "315px" : "0px" }}
+                            transition={{ duration: inputValue1 }}
                         />
                         <div onClick={() => setOpen3(!isOpen3)}
                             className="boxBat"
-                        >13 SEK</div>
+                        >{ inputValue1 }</div>
                     </div>
 
 
@@ -71,6 +75,13 @@ const Tajmer = () => {
 
 
                 </div>
+
+                <div className="inputText" > Upiši broj sekundi <span/>
+                            <input type="number" className="inputPlace"
+                            name="broj"
+                                value={inputValue1}
+                                onChange={handleChangeValue1} />
+                        </div>
             </div>
 
 
