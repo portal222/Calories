2b043c6b167c4513266a22dc55dcefc2017f7b35
portal222 <dvaportala@gmail.com>
2023-09-08 19:38:02 +0200

    5 row

--- src/components/Tajmer.js
+++ src/components/Tajmer.js
@@ -8,11 +8,16 @@ const Tajmer = () => {
     const [isOpen1, setOpen1] = useState(false)
     const [isOpen2, setOpen2] = useState(false)
     const [isOpen3, setOpen3] = useState(false)
+    const [isOpen4, setOpen4] = useState(false)
     const [inputValue1, setInputValue1] = useState(0);
+    const [inputValue2, setInputValue2] = useState(0);
 
 
     const handleChangeValue1 = (e) => {
         setInputValue1(e.target.value)
+    }
+    const handleChangeValue2 = (e) => {
+        setInputValue2(e.target.value)
     }
 
     return (
@@ -25,13 +30,17 @@ const Tajmer = () => {
                             className="box"
                             layout
                             style={{ height: isOpen ? "315px" : "0px" }}
-                            transition={{ duration: 5 }}
+                            transition={{
+                                ease: "linear",
+                                duration: 5,
+                                repeat: Infinity,
+                            }}
 
                         />
                         <div onClick={() => setOpen(!isOpen)}
                             className="boxBat"
                         >5 sek</div>
-                       
+
                     </div>
 
                     <div>
@@ -39,7 +48,11 @@ const Tajmer = () => {
                             className="box1"
                             layout
                             style={{ height: isOpen1 ? "315px" : "0px" }}
-                            transition={{ duration: 10 }}
+                            transition={{
+                                ease: "linear",
+                                duration: 10,
+                                repeat: Infinity
+                            }}
                         />
                         <div onClick={() => setOpen1(!isOpen1)}
                             className="boxBat"
@@ -50,7 +63,11 @@ const Tajmer = () => {
                             className="box2"
                             layout
                             style={{ height: isOpen2 ? "315px" : "0px" }}
-                            transition={{ duration: 20 }}
+                            transition={{
+                                ease: "linear",
+                                repeat: Infinity,
+                                duration: 20
+                            }}
                         />
                         <div onClick={() => setOpen2(!isOpen2)}
                             className="boxBat"
@@ -61,27 +78,56 @@ const Tajmer = () => {
                             className="box3"
                             layout
                             style={{ height: isOpen3 ? "315px" : "0px" }}
-                            transition={{ duration: inputValue1 }}
+                            transition={{
+                                ease: "linear",
+                                duration: inputValue1,
+                                repeat: Infinity,
+
+                            }}
                         />
                         <div onClick={() => setOpen3(!isOpen3)}
                             className="boxBat"
-                        >{ inputValue1 }</div>
+                        >{inputValue1}</div>
                     </div>
+                    <div>
+                        <motion.div
+                            className="box4"
+                            layout
+                            style={{ height: isOpen4 ? "315px" : "0px" }}
+                            transition={{
+                                ease: "linear",
+                                duration: inputValue2,
+                                repeat: Infinity,
 
-
-
-
-
-
+                            }}
+                        />
+                        <div onClick={() => setOpen4(!isOpen4)}
+                            className="boxBat"
+                        >{inputValue2}</div>
+                    </div>
 
                 </div>
 
-                <div className="inputText" > Upiši broj sekundi <span/>
-                            <input type="number" className="inputPlace"
-                            name="broj"
-                                value={inputValue1}
-                                onChange={handleChangeValue1} />
-                        </div>
+                <div className="inputText" > Upiši broj sekundi <span />
+                    <input type="number" className="inputPlace"
+                        name="broj"
+                        value={inputValue1}
+                        onChange={handleChangeValue1} />
+                </div>
+                <div className="inputText" > Upiši broj sekundi <span />
+                    <input type="number" className="inputPlace"
+                        name="broj"
+                        value={inputValue2}
+                        onChange={handleChangeValue2} />
+                </div>
+                <div
+                    onClick={() => [setOpen4(!isOpen4), setOpen3(!isOpen3), setOpen2(!isOpen2),
+                    setOpen1(!isOpen1), setOpen(!isOpen)]}
+                    className="boxBatAll"
+                >
+
+                    sve</div>
+
             </div>
 
 
