df083a75beec25b474efd18653e9de14678a647c
portal <dva portala@gmail>
2023-11-15 00:08:58 +0100

    digitron

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@
 
 
 import React from "react";
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import "./scss/main.scss";
 
 
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App
+     number={10}
+     title="String from index file" 
+     text="Text from index file" />);
+
+// const container = document.getElementById("root");
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App tab="home" />);
 
 
 
